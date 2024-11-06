a8e2bd753482779d74bcd21de42505a21435994b
kimyunong <money32132@naver.com>
2024-11-06 13:43:30 +0900

    todo be v2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 require('dotenv').config();
 const app = express();
-const MONGODB_URI_PROD= process.env.MONGODB_URI_PROD; 
+const MONGODB_URI_PROD= process.env.MONGODB_URI_PROD;
 
 app.use(cors());
 app.use(bodyParser.json());
